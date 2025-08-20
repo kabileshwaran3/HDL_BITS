84f24e653aca39faf6df78551871a4a729984655
kabileshwaran <skabilesh231@gmail.com>
2025-08-20 21:35:13 +0530

    Create 25.always_block.v

---
+++ Procedures/25.always_block.v
@@ -0,0 +1,14 @@
+module top_module(
+    input clk,
+    input a,
+    input b,
+    output wire out_assign,
+    output reg out_always_comb,
+    output reg out_always_ff   );
+    always@ (posedge clk) 
+        out_always_ff = a^b;
+    always@ (*) 
+        out_always_comb = a^b;
+    assign out_assign = a^b;
+
+endmodule
